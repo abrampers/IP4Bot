de8115c4edeed97d614aa60a701d059bd6154e82
Abram Situmorang <abram.perdanaputra@gmail.com>
2018-11-06 14:34:22 +0700

    Match using regex

--- bot.js
+++ bot.js
@@ -2,24 +2,23 @@ const Telegraf = require('telegraf')
 const Extra = require('telegraf/extra')
 const Markup = require('telegraf/markup')
 
-const LT_URL = 'https://fuzzy-octopus-71.localtunnel.me'
-const BOT_TOKEN = '747892200:AAESi-TqEW25rLjhgQvRaZrkXL80uBlDuNU';
+const BOT_TOKEN = '747892200:AAESi-TqEW25rLjhgQvRaZrkXL80uBlDuNU'
 const bot = new Telegraf(BOT_TOKEN)
 
 // Helpers
 String.prototype.contains = function(words) {
-    if (this.trim().split(' ').length == words.length) {
+    if (this.trim().split(' ').length >= words.length) {
         let wordsMatch = true
         const text = this.toLowerCase()
         for (let word of words) {
-            if (Array.isArray(words)) {
+            if (Array.isArray(word)) {
                 let optionOccurs = false
                 for (let option of word) {
-                    optionOccurs = optionOccurs || text.includes(option.toLowerCase())
+                    optionOccurs = optionOccurs || text.match(option)
                 }
                 wordsMatch = wordsMatch && optionOccurs
             } else {
-                wordsMatch = wordsMatch && text.includes(word.toLowerCase())
+                wordsMatch = wordsMatch && text.match(word)
             }
         }
         return wordsMatch
@@ -48,18 +47,17 @@ bot.on('message', (ctx) => {
     // ctx.telegram.sendCopy(ctx.from.id, ctx.message, Extra.markup(keyboard))
     // ctx.telegram.sendMessage(ctx.from.id, 'kuntul')
     const text = ctx.message.text
-    if (text.contains([['set', 'change'], 'daily', 'reminder'])) {
-        ctx.reply('Daily reminder')
-    } else if (text.contains([['set', 'change'], 'class', 'reminder'])) {
-        ctx.reply('Class reminder')
-    } else if (text.contains([['set', 'change'], ['homework', 'quiz', 'deadline'], 'reminder'])) {
+    if (text.contains([[/(set)/i, /(change)/i], /(daily)/i, /(reminder)/i, ])) {
+        const num = text.replace ( /[^\d.]/g, '' );
+        ctx.reply('Alright! We’ll remind you at ' + num + ' everyday.')
+    } else if (text.contains([[/(set)/i, /(change)/i], /(class)/i, /(reminder)/i])) {
+        const num = text.replace ( /[^\d.]/g, '' );
+        ctx.reply('Alright! We’ll remind you ' + num + ' minutes before class.')
+    } else if (text.contains([[/(set)/i, /(change)/i], [/(homework)/i, /(quiz)/i, /(deadline)/i], /(reminder)/i])) {
         ctx.reply('HWQ reminder')
     } else {
         ctx.reply('Sorry, I don\'t understand that. ☹️')
     }
 })
 
-// npm install -g localtunnel && lt --port 3000
-// bot.telegram.setWebhook(LT_URL + '/asu')
-// bot.startWebhook('/asu', null, 3000)
 bot.startPolling()
