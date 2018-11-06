d76ae670329c0172de4e629d134018ed8a023449
Faza Fahleraz <ffahleraz@gmail.com>
2018-11-06 10:45:25 +0700

    Add contains() method to String prototype

--- bot.js
+++ bot.js
@@ -2,9 +2,19 @@ const Telegraf = require('telegraf')
 const Extra = require('telegraf/extra')
 const Markup = require('telegraf/markup')
 
-const LT_URL = 'https://black-rattlesnake-84.localtunnel.me'
+const LT_URL = 'https://fuzzy-octopus-71.localtunnel.me'
 const BOT_TOKEN = '747892200:AAESi-TqEW25rLjhgQvRaZrkXL80uBlDuNU';
 const bot = new Telegraf(BOT_TOKEN)
+
+// Helpers
+String.prototype.contains = function(words) {
+    let match = true
+    const text = this.toLowerCase()
+    for (let word of words) {
+        match = text.includes(word.toLowerCase())
+    }
+    return match
+}
 
 // Keyboards
 const keyboard = Markup.inlineKeyboard([
@@ -25,13 +35,16 @@ bot.action('delete', ({ deleteMessage }) => deleteMessage())
 bot.on('message', (ctx) => {
     // ctx.telegram.sendCopy(ctx.from.id, ctx.message, Extra.markup(keyboard))
     // ctx.telegram.sendMessage(ctx.from.id, 'kuntul')
-    if (ctx.message.text.includes('set') && ctx.message.text.includes('daily reminder')) {
-        ctx.reply('asu bangsat')
+    const text = ctx.message.text
+    if (text.contains(['set', 'daily', 'reminder'])) {
+        ctx.reply('Okay, I will remind you every huyu')
     } else {
         ctx.reply('belom ngerti bang')
     }
 })
 
 // npm install -g localtunnel && lt --port 3000
-bot.telegram.setWebhook(LT_URL + '/asu')
-bot.startWebhook('/asu', null, 3000)
+// bot.telegram.setWebhook(LT_URL + '/asu')
+// bot.startWebhook('/asu', null, 3000)
+
+bot.startPolling()
